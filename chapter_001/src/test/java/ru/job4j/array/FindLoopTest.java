8850fdabe9afdce8f5de0f490533f922b10b2d52
AndgreyEfremenkov <andgrearmy@gmail.com>
2019-10-17 23:15:44 +0300

    Поиск индекса в диапазоне.[#183733]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -7,42 +7,73 @@ import static org.junit.Assert.assertThat;
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHas5Then0(){
-        FindLoop find=new FindLoop();
-        int[] input=new int[]{5,10,3};
-        int value= 5;
-        int result= find.indexOf(input, value);
-        int expect= 0;
-        assertThat(result,is(expect));
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
     }
 
     @Test
-    public void whenArrayHas8Then3(){
-        FindLoop find=new FindLoop();
-        int[] input=new int[]{6,12,3,8,22};
-        int value= 8;
-        int result= find.indexOf(input, value);
-        int expect= 3;
-        assertThat(result,is(expect));
+    public void whenArrayHas8Then3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{6, 12, 3, 8, 22};
+        int value = 8;
+        int result = find.indexOf(input, value);
+        int expect = 3;
+        assertThat(result, is(expect));
     }
 
     @Test
-    public void whenArrayHas17Then1(){
-        FindLoop find=new FindLoop();
-        int[] input=new int[]{5,17,33,23,57};
-        int value= 17;
-        int result= find.indexOf(input, value);
-        int expect= 1;
-        assertThat(result,is(expect));
+    public void whenArrayHas17Then1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 17, 33, 23, 57};
+        int value = 17;
+        int result = find.indexOf(input, value);
+        int expect = 1;
+        assertThat(result, is(expect));
     }
 
     @Test
-    public void whenArrayHas4ThenMinus1(){
-        FindLoop find=new FindLoop();
-        int[] input=new int[]{5,17,3,23,57};
-        int value= 4;
-        int result= find.indexOf(input, value);
-        int expect= -1;
-        assertThat(result,is(expect));
+    public void whenArrayHas4ThenMinus1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 17, 3, 23, 57};
+        int value = 4;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasLength5Then0() {
+        int[] input = new int[]{5, 10, 3};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFinishGreaterArray() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 8;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
     }
 }
