3e1ab7c6ebc093a9a92186e665722bad0270da3d
AndgreyEfremenkov <andgrearmy@gmail.com>
2019-11-22 11:11:49 +0300

    work at home

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Profession.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+public class Profession {
+    String name;
+
+    String surname;
+
+    String education;
+
+    int birthday;
+
+    public String getName() {
+        return this.name;
+    }
+    public String getSurname() {
+        return this.surname;
+    }
+    public String getEducation() {
+        return this.education;
+    }
+    public int getBirthday() {
+        return this.birthday;
+    }
+}
