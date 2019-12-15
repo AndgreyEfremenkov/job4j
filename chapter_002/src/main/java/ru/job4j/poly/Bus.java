53d19a00740a138bda1fb8160004f9119b71b02d
AndgreyEfremenkov <andgrearmy@gmail.com>
2019-12-15 21:37:05 +0300

    Интерфейс Input[#183827]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,26 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    private int passengers = 0;
+    private double fuel = 0;
+    private double money = 0;
+
+    @Override
+    public void ride() {
+        this.fuel-=5;
+    }
+
+    @Override
+    public void passengers(int numPassengers) {
+        this.passengers += numPassengers;
+        this.money +=(numPassengers*25);
+    }
+
+    @Override
+    public double fill(double fuel) {
+        this.fuel +=fuel;
+        return this.money-=(fuel*10);
+    }
+
+}
---
+++ chapter_002/src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+   void ride();
+   void passengers(int numPassengers);
+   double fill(double fuel);
+}
